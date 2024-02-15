e33e940da7b448919e435f3cb52a26baf4375d9d
gellerjulia <74266997+gellerjulia@users.noreply.github.com>
2024-02-15 12:25:14 -0500

    update

---
+++ script.js
@@ -0,0 +1,4 @@
+console.log("Hello, command-line!");
+mongoimport --host 27017 --db ehr_db --collection ehr --file '../C:/Users/J-Dog/24Spring/Large_Info/DS4300_mongodb/data/000/000a336a-41c8-4e46-8526-1e502346e28f.json';
+
+// C:/Users/J-Dog/24Spring/Large_Info/DS4300_mongodb/data/000/000a336a-41c8-4e46-8526-1e502346e28f.json
